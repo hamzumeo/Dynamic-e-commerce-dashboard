7ba2019c4d54a295d51d555f061f063d29725847
Hamza Amjad <meohamza122@gmail.com>
2025-12-02 16:14:44 +0500

    Update project with latest changes

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("shop.urls")),
+    path('admin/', admin.site.urls),
+
+    # Shop app routes
+    path('', include('shop.urls')),
+
+    # Django built-in auth URLs (login, logout, password change, reset)
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
